5d15e487926b0bd040a99b18323bc7781ca45a50
Daniel Ruiz <danuelruiz.digital@gmail.com>
2019-01-26 12:12:38 -0500

    Versión 1.3.0 (Recuperación de resultados)

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -4,6 +4,7 @@ class Resultados extends Component {
   constructor() {
     super();
     this.state = {
+      datos: [],
       carga: true
     };
     this.tiempoCarga = this.tiempoCarga.bind(this);
@@ -15,9 +16,25 @@ class Resultados extends Component {
   }
 
   render() {
-   
     const nResultados = this.props.datos ? this.props.datos.length : 0;
-    if(nResultados){this.tiempoCarga()}
+    if(nResultados){this.tiempoCarga();}
+
+        const resultados = (this.props.datos !== undefined) ? this.props.datos.map((data, i) => {
+        return (
+          <div className="col-md-4">
+            <div className="card mt-4">
+              <div className="card-body">
+                <p>{ data.Poster }</p>
+              </div>
+              <div className="card-footer">
+                <p>{ data.Title }</p>
+                <p>{ data.Year }</p>
+              </div>
+            </div>
+          </div>
+        )
+      }) : null
+
     return (
       <div className="App">
         <div className={this.props.carga === false ? 'd-none' : 'd-block'}>
@@ -27,8 +44,9 @@ class Resultados extends Component {
             <hr />
             <p>{ nResultados } results for "Batman"</p>
             <hr />
+            <div></div>
         </div>
-
+        <div>{resultados}</div>
 
       </div>
     );
